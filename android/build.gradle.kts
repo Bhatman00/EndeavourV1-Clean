22667c87566f584984e3880e7c14bb922b4e6382
OmBhatt15 <bhattom76@gmail.com>
2026-05-24 22:58:26 +1000

    Add Google Services classpath to build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,6 +6,8 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.4.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        // ADD THIS LINE
+        classpath("com.google.gms:google-services:4.4.2")
     }
 }
 
