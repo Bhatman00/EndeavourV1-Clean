c40c7f1dd4bfa75ed3c348e225cbd441710d70b4
OmBhatt15 <bhattom76@gmail.com>
2026-05-25 09:14:49 +1000

    Changed google service version

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:8.3.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
         // ADD THIS LINE
-        classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.google.gms:google-services:4.3.15")
     }
 }
 
